2e925fb5e9ecfa9fef3ee99fb4999ed9c7cb6b09
Ryan Van Valkenburg <ryanv@trustnota.com>
2021-07-22 18:16:39 -0700

    Added syles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+
+});
 export default () => {
     return (
+        
         <BrowserRouter>
-            <div>
-                <Header />
-                <hr />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <hr />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
